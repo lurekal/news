c5ac71df2ca74056cf3799bb7b9a57841ccb31ab
lurekal <lurekal@gmail.com>
2021-04-06 18:34:17 +0900

    api 연동

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [data,setData]=useState(null);
+  const onClick=async()=>{
+    try{
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=93424d526b064f2eba25aa4eb3ecfcd4');
+      setData(response.data)
+    }catch(e){
+      console.log(e)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <button onClick={onClick}>불러오기</button>
     </div>
+    {data && <textarea row={7} value={JSON.stringify(data,null,2)}/>}
+  </div>
   );
 }
 
